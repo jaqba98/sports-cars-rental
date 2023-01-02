18bc53b73f41a85a9e2fcced980403cbbe5faf8e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2023-01-02 20:57:50 +0100

    refactor the wenbsite nav

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,6 +1,6 @@
 const hamburgerEL = document.getElementById("hamburger");
 const topNavEL = document.getElementById("top-nav");
 
-const toggleTopNav = () => topNavEL.classList.toggle("top-nav-open");
-
-hamburgerEL.addEventListener("click", () => toggleTopNav());
+hamburgerEL.addEventListener("click", () => {
+  topNavEL.classList.toggle("top-nav-open");
+});
